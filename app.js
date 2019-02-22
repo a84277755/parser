989da092dd60d856a843cb01073b6dbf22a42e09
a84277755 <a84277755@gmail.com>
2019-02-23 02:00:39 +0300

    added searcher by tag

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const {getPageRequest} = require('./controllers/request-http');
-const {createVirtualDOM} = require('./controllers/parse-tags');
+const {createVirtualDOM, findClosestTag} = require('./controllers/parse-tags');
 
-getPageRequest('http://realtmsk.ru/uslugi/soprovozhdenie-sdelki').then(createVirtualDOM).then(virtualDOM => {
-    console.log('virtualDOM FROM DOM: ', virtualDOM.document.documentElement.querySelector('h1').textContent);
+// getPageRequest('http://realtmsk.ru/uslugi/soprovozhdenie-sdelki').then(createVirtualDOM).then(virtualDOM => {
+//     console.log('virtualDOM FROM DOM: ', virtualDOM.document.documentElement.querySelector('h1').textContent);
+// }).catch(e => console.log('err: ', e));
+
+getPageRequest('http://realtmsk.ru/ceny').then(htmlCode => {
+    const tag = findClosestTag('90.000')(htmlCode);
+    console.log('tag >> ', tag);
 }).catch(e => console.log('err: ', e));
--- controllers/parse-tags.js
+++ controllers/parse-tags.js
@@ -7,8 +7,12 @@ const createVirtualDOM = (HTMLCode) => {
     } catch ( error ) {
         return Promise.reject("Ошибка создания виртуального DOM: " + error);
     }
-    
 };
 
+const findClosestTag = searchText => HTMLCode => {
+    const regExp = new RegExp(`<([\\d\\w]{1,10}).{0,100}>.{0,10}${searchText}.{0,10}<\/.{1,10}>`);
+    const result = HTMLCode.match(regExp);
+    return result ? result[1] : null;
+};
 
-module.exports = {createVirtualDOM};
+module.exports = {createVirtualDOM, findClosestTag};
