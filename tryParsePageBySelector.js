2da5e51ed83774266e87e971a03a8e43805383bc
Artem Gorokhovskii <a84277755@gmail.com>
2019-03-23 13:36:05 +0300

    modified searching by selector instrument

--- tryParsePageBySelector.js
+++ tryParsePageBySelector.js
@@ -1,17 +1,42 @@
 const puppeteer = require('puppeteer');
-const findTextBySelector = async (url, selector) => {
+
+const URL = 'https://www.cian.ru/sale/flat/202220058/';
+const elemsToSearch = [
+    {
+        description: 'Дата обновления',
+        selector: 'div[class="a10a3f92e9--container--3nJ0d"]'
+    },
+    {
+        description: 'Заголовок обновления',
+        selector: 'h1[class="a10a3f92e9--title--2Widg"]'
+    },
+    {
+        description: 'Цена обновления',
+        selector: 'td[class="price_history_widget-event-price-1hxoWz1dS"]'
+    },
+    {
+        description: 'Адрес обновления',
+        selector: 'a[class="a10a3f92e9--link--1t8n1 a10a3f92e9--address-item--1clHr"]'
+    },
+    {
+        description: 'Телефон обновления',
+        selector: 'a[class="a10a3f92e9--phone--3XYRR"]'
+    },
+    {
+        description: 'Описание обновления',
+        selector: 'p[itemprop="description"][class="a10a3f92e9--description-text--1_Lup"]'
+    }
+];
+
+const findTextBySelector = async ({description, selector}) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
-    await page.waitFor(500);
+    await page.goto(URL);
     await page.waitForSelector(selector, { timeout: 0 });
-    const pageContent = await page.$eval(selector, 
-    selector => selector.innerHTML);
+    const pageContent = await page.$$eval(selector, nodes => nodes.map(n => n.innerText || n.innerHTML));
     browser.close();
-    console.log('URL: ' + url + '\nСелектор: ', selector + '\n Текст: ' + pageContent);
+    console.log(description + ': ' + pageContent.join(', '));
 }
 
-findTextBySelector(
-    'https://www.cian.ru/sale/flat/202804816/', 
-    'a[target=\"_blank\"][rel=\"noopener\"][class=\"a10a3f92e9--link--1t8n1 a10a3f92e9--link--2Aiso\"]'
-)
+console.log('URL: ', URL);
+elemsToSearch.forEach(findTextBySelector);
